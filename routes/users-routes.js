a13f52a86ba019e9c5af377c7df0c19e09fac525
chakradharkotagiri <chakradharkotagiri111@gmail.com>
2024-08-29 20:21:09 +0530

    configured cors origin list

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -22,31 +22,3 @@ router.post(
 router.post("/login", usersControllers.login);
 
 module.exports = router;
-
-// const express = require("express");
-
-// const router = express.Router();
-
-// const DUMMY_PLACES = [
-//     {
-//       id: "p1",
-//       title: "Empire State Building",
-//       description: "One of the most iconic landmarks in the world",
-//       location: {
-//         lat: 40.7484405,
-//         lng: -73.9856654,
-//       },
-//       address: "350 5th Ave, New York, NY 10118, USA",
-//         creator:'u1 '
-//     },
-//   ];
-
-//   router.get("/:uid", (req, res, next) => {
-//     const userId = req.params.uid;
-//     const user = DUMMY_PLACES.find(u => {
-//         return u.id === userId;
-//     })
-//       res.json({ user });
-//     });
-
-//     module.exports = router;
