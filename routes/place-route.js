8e73dbf26ce08dd923696862d0028c17d5b486d3
chakradharkotagiri <chakradharkotagiri111@gmail.com>
2025-12-18 13:26:42 +0530

    syntax error fixed

--- routes/place-route.js
+++ routes/place-route.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { check } = require("express-validator");
 
-const placesControllers = require("../controllers/places-controller");
+const placesControllers = require("../controllers/places-controllers");
 const checkAuth = require("../middleware/check-auth");
 
 const router = express.Router();
