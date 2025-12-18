091122c3c1afeb6135d0c0919e0197f621b345c5
chakradharkotagiri <chakradharkotagiri111@gmail.com>
2025-12-18 13:21:50 +0530

    Routes fixed

--- routes/place-route.js
+++ routes/place-route.js
@@ -1,37 +1,34 @@
 const express = require("express");
 const { check } = require("express-validator");
-// const fileUpload = require('../middleware/file-upload')
-const checkAuth = require('../middleware/check-auth')
 
-
-const placesControllers = require("../controllers/places-controllers");
-const fileUpload = require("../middleware/file-upload");
+const placesControllers = require("../controllers/places-controller");
+const checkAuth = require("../middleware/check-auth");
 
 const router = express.Router();
 
-router.get("/user/:uid", placesControllers.getPlacesByuserId);
-
+router.get("/user/:uid", placesControllers.getPlacesByUserId);
 router.get("/:pid", placesControllers.getPlaceById);
+
 
 router.use(checkAuth);
 
 router.post(
   "/",
-  checkAuth,
   [
     check("title").not().isEmpty(),
     check("description").isLength({ min: 5 }),
     check("address").not().isEmpty(),
-    check("image").isString(), 
+    check("image").isString(),
   ],
   placesControllers.createPlace
 );
 
-
-
 router.patch(
   "/:pid",
-  [check("title").not().isEmpty(), check("description").isLength({ min: 5 })],
+  [
+    check("title").not().isEmpty(),
+    check("description").isLength({ min: 5 }),
+  ],
   placesControllers.updatePlace
 );
 
